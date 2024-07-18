f437bef40cbd1c0ee9d4cfe984e5039cff1f01ee
yhanyi <yeohhanyi0916@gmail.com>
2024-07-18 08:59:50 +0800

    Updated typewriting text component

--- src/components/typewriter-text.tsx
+++ src/components/typewriter-text.tsx
@@ -1,51 +1,30 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import { AnimatePresence, motion } from "framer-motion";
-
-export default function TypewriterText() {
-  return (
-    <div className="px-4 py-24 text-center md:py-36">
-      <h3 className="text-3xl font-medium sm:text-4xl md:text-5xl lg:text-6xl my-20 gap-5 flex items-center justify-center">
-        Hello, I&apos;m Han Yi!
-      </h3>
-      <h3 className="text-xl font-medium sm:text-2xl md:text-3xl lg:text-4xl my-20">
-        CS & Maths @ NUS
-      </h3>
-      <h3 className="text-xl font-medium  sm:text-2xl md:text-3xl lg:text-4xl">
-        I&apos;m interested in
-        <AnimatedText
-          phrases={[
-            "Machine Learning",
-            "Quantitative Development",
-            "Artificial Intelligence",
-            "Algorithm Theory",
-            "Software Engineering",
-          ]}
-        />
-      </h3>
-    </div>
-  );
-}
 
 const ONE_SECOND = 1000;
 const WAIT_TIME = ONE_SECOND * 2;
 const STAGGER = 0.025;
 
-const AnimatedText = ({ phrases }: any) => {
+export default function AnimatedText({ phrases, classNames }: any) {
   const countRef = useRef(0);
-  const [active, setActive] = useState(0);
+  const activeRef = useRef(0);
+  const [activePhrase, setActivePhrase] = React.useState(phrases[0]);
 
   useEffect(() => {
     const intervalRef = setInterval(() => {
-      setActive((pv) => (pv + 1) % phrases.length);
+      activeRef.current = (activeRef.current + 1) % phrases.length;
+      setActivePhrase(phrases[activeRef.current]);
     }, WAIT_TIME);
 
     return () => clearInterval(intervalRef);
   }, [phrases]);
 
   return (
-    <div className="flex flex-wrap items-center justify-center gap-1.5 sm:gap-2 md:gap-2.5 lg:gap-4">
+    <div
+      className={`flex flex-wrap items-center justify-center gap-2 ${classNames}`}
+    >
       <AnimatePresence mode="popLayout">
-        {phrases[active].split(" ").map((word: string, wordIndex: number) => {
+        {activePhrase.split(" ").map((word: string, wordIndex: number) => {
           if (wordIndex === 0) {
             countRef.current = 0;
           }
@@ -89,4 +68,4 @@ const AnimatedText = ({ phrases }: any) => {
       </AnimatePresence>
     </div>
   );
-};
+}
